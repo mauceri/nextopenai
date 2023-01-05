01a007f405eafe4978ed1d1e2e5494d9262b8c86
Christian Mauceri <christian@mauceri.me>
2023-01-05 18:41:56 +0100

    Nettoyage en grand

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,10 +5,8 @@ import Dialogue from "../components/dialogue";
 export default function Home() {
   return (
     <Layout>
-      <div className="flex flex-col h-screen overflow-hidden">
         <Header />
         <Dialogue />
-      </div>
     </Layout>
   )
  
