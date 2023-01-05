f23f31e0466c9b377a9c83423d056cb348fbbe7b
Mauceri <christian@mauceri.me>
2023-01-05 20:25:31 +0100

    Update dialogue.tsx

--- components/dialogue.tsx
+++ components/dialogue.tsx
@@ -47,7 +47,7 @@ export default function Dialogue() {
         <div className="flex flex-col h-screen ">
           <div
             id="messages"
-            className="overflow-y-scroll h-screen mt-4 flex flex-col space-y-4 p-3 scrollbar-thumb-blue scrollbar-thumb-rounded scrollbar-track-blue-lighter scrollbar-w-2 scrolling-touch"
+            className="overflow-y-scroll height:50% mt-4 flex flex-col space-y-4 p-3 scrollbar-thumb-blue scrollbar-thumb-rounded scrollbar-track-blue-lighter scrollbar-w-2 scrolling-touch"
           >
             {messages.map((message, index) => {
               return message.from === "ai" ? (
